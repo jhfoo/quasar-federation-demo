4aa33152c9ab852ed4a111e6dd675e9877e3002f
Foo JH <foojihaw@hotmail.com>
2023-02-20 17:20:11 -0600

    TEST: script to tag during commit

---
+++ quasar-fed-demo-host/src/components/TheWelcome.vue
@@ -0,0 +1,21 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    Vue’s<MyButton/>
+  </WelcomeItem>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+const MyButton = defineAsyncComponent(() => import('remote_app/Button'));
+
+export default defineComponent({
+  name: 'TheWelcome',
+  components: {
+    MyButton,
+  }
+})
+</script>
